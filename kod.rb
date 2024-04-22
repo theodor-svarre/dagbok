f2ae17c36bebe34e5e2dff29dcd347a3509a14fc
theodor-svarre <152278794+theodor-svarre@users.noreply.github.com>
2024-04-22 14:33:48 +0200

    lektion 2

--- kod.rb
+++ kod.rb
@@ -1,11 +1,54 @@
-# def filesort(file)
-#     print "title"
-#     title = gets.chomp+Time.now.strftime()
-#     new_f = File.open(title, "w")
-#     print "fsuif"
-#     content = gets.chomp
-#     new_f.puts(title"\n"+content)
-#     new_f.close 
-# end 
+def dagobok(svar)
+    while 1 > 0 
+        i = 1 
 
-# File.foreach("test.txt") { |line| puts line }
+        if svar.downcase == "stäng dagboken"
+            return 
+        end 
+
+        while svar.downcase != "ny sida" && svar.downcase != "läs gammal"
+            puts "Jag upprepar, ny sida eller läs gammal?"
+            svar = gets.chomp
+        end 
+    
+        if svar.downcase == "ny sida"
+            puts "Vad ska den heta?"
+                title = gets.chomp
+                new_file = File.open("#{title}.txt", "w")
+                while i > 0 
+                    ny_text = gets.chomp 
+                    if ny_text == "stäng sidan"
+                    end 
+                    if i == 1 
+                    new_file.puts(ny_text)
+                    end 
+                end 
+                new_file.close 
+        end 
+       
+    
+        if svar.downcase == "läs gammal"
+            puts "vilken sida"
+            sida = gets.chomp
+            page = File.open("#{sida}.txt", "r")
+            puts page.read()
+            page.close()
+            page = File.open("#{sida}.txt", "a")
+            while i > 0 
+                ny_text = gets.chomp 
+                page.puts(ny_text)
+                if ny_text == "stäng sidan"
+                    i = 0 
+                end 
+            end 
+            page.close()
+          end 
+        puts "Ny sida eller läs gammal"
+        svar = gets.chomp
+    end 
+end 
+
+puts "Ny sida eller läs gammal"
+svar = gets.chomp
+puts dagobok(svar)
+    

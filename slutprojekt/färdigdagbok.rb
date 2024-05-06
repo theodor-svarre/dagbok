3bcf7a697f50060b937508cd8950a9d38818e7c6
VileKindstrand <vile.kindstrand@elev.ga.ntig.se>
2024-05-06 22:28:27 +0200

    final dok

--- slutprojekt/färdigdagbok.rb
+++ slutprojekt/färdigdagbok.rb
@@ -56,18 +56,23 @@ def new_file()
     #----------------------------------------------------------------------------
     
     def date_check()
-    # Beskrivning:         Hjälpfunktion som kollar vilket datum det är. Om en folder med sagda datum inte finns skapar den det
-    # Variabel 1:          String - Tar ut vilket år det är m.h.a Time.now.strftime() metod
-    # Variabel 2:          String - Tar ut vilken månad det är m.h.a Time.now.strftime() metod
-    
-    # Return:              Om datum stämmer 
-    # Exempel:  
-    
-    
-    
-    
-    # Datum:               06-05-2024
-    # Namn:                Vile Kindstrand, Theodor Svarre
+# Beskrivning:         Hjälpfunktion som kollar vilket datum det är. Om en folder med sagda datum inte finns skapar den det
+# Variabel 1:          String - "year" Tar ut vilket år det är m.h.a Time.now.strftime() metod
+# Variabel 2:          String - "month" Tar ut vilken månad det är m.h.a Time.now.strftime() metod
+# Argument 1:          Klass - Vi använder oss ej av argument i den här funktionen eftersom att de enda variablerna är beroende av tiden, vilket hämtas i början av funktionen. 
+
+# Return:              Om folders med datum ej finns (år respektive månad) skapas nödvändiga folders, annars sker ingenting
+
+# Exempel:  
+# puts Dir.exist?("years/#{year}") ==> False ==> skapar ny folder för år
+# puts Dir.exist?("years/#{year}/#{month}") ==> True ==> skip
+
+
+
+
+
+# Datum:               06-05-2024
+# Namn:                Vile Kindstrand, Theodor Svarre
         year = Time.now.strftime("%Y")
         month = Time.now.strftime("%m")
     
@@ -84,6 +89,63 @@ def new_file()
 #----------------------------------------------------------------------------
 
     def read()
+# Beskrivning:         Hjälpfunktion som kollar vilket datum det är. Om en folder med sagda datum inte finns skapar den det
+# Variabel 1:          String - "year" Tar ut vilket år det är m.h.a Time.now.strftime() metod
+# Variabel 2:          String - "month" Tar ut vilken månad det är m.h.a Time.now.strftime() metod
+# Argument:          Klass - Vi använder oss ej av argument i den här funktionen eftersom att de enda variablerna är beroende av tiden, vilket hämtas i början av funktionen. 
+
+# Return:              Om folders med datum ej finns (år respektive månad) skapas nödvändiga folders, annars sker ingenting
+
+# Exempel:  
+
+# välj mellan följande år:
+# .
+# ..
+# 2022
+# 2024
+# input: 2300
+# År 2300 finns inte. Försök igen.
+# välj mellan följande år:        
+# .
+# ..
+# 2022
+# 2024
+# input: ergergf
+# År ergergf finns inte. Försök igen.
+# välj mellan följande år:
+# .
+# ..
+# 2022
+# 2024
+# input: ?(#/¤&
+# År ?(#/¤& finns inte. Försök igen.
+# välj mellan följande år:
+# .
+# ..
+# 2022
+# 2024
+# input: 2024
+# välj mellan följande månader:
+# .
+# ..
+# 05
+# input: 05
+# välj mellan följande sidor:
+# .
+# ..
+# rumpnisse [06-05-2024]
+# test1 [06-05-2024]
+# Vileh [06-05-2024]
+# input: Vileh [06-05-2024]
+# den här dagen skrev du följande:
+# Vileh [06-05-2024]
+#  (innehåll nedan)
+# Jätebra
+
+
+
+# Datum:               06-05-2024
+# Namn:                Vile Kindstrand, Theodor Svarre
     
         i = 0 
     
